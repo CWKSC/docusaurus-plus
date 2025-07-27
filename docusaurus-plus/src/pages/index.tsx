a0cbd7503e92c1ce286b4119ccf7b73453577438
CWKSC <53114952+CWKSC@users.noreply.github.com>
2025-07-27 16:46:19 +0800

    Enhance Home component layout and add personal introduction

--- docusaurus-plus/src/pages/index.tsx
+++ docusaurus-plus/src/pages/index.tsx
@@ -1,14 +1,38 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout
+            title={`Hello from ${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />">
 
-    </Layout>
-  );
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '32px',
+                    paddingTop: '64px',
+                    gap: '16px',
+                }}>
+
+                <img style={{
+                    verticalAlign: 'middle',
+                    width: '128px',
+                    height: '128px',
+                    borderRadius: '50%'
+                }} src="./img/icon.jpg" alt="Avatar" />
+
+                <h1> About me 關於我 </h1>
+
+                <p> 一個什麼都學，但什麼都不精通的人 </p>
+
+            </div>
+
+        </Layout>
+    );
 }
